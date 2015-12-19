3d97656183830fb0c5b1380a7ba499287c13b968
Shayan Araghi <shayanaraghi@live.com>
2015-12-19 15:36:11 +0330

    Material Toast Javascript
    Material Toast Javascript Js

---
+++ Material_Toast.js
@@ -0,0 +1,68 @@
+
+if (typeof jQuery == 'undefined') {
+	console.log("Notice :You Have To Add Jquery Befor Using This Library");
+}else{
+	$(document).on("click",".toast_material",function(){
+		if($('.toast_open').length){
+			$('.toast_material').removeClass("fadeInUp");
+			$('.toast_material').addClass("fadeOutUp");
+			setTimeout(function() {
+				$('.toast_material').remove();
+			}, 800);
+		}
+	});
+}
+function isset(variable) {
+    if (typeof variable !== "undefined" && variable !== null) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function Toast_Material(options){
+	if(isset(options.position)){
+		var positionStyle = options.position + ":16px";
+	}
+	else{
+		var positionStyle = "left : 16px";
+	}
+	if(isset(options.align)){
+		var alignStyle = options.align;
+		if(alignStyle == "right"){
+			var directionStyle = "rtl";
+		}
+		else if(alignStyle == "left"){
+			var directionStyle = "ltr";
+		}
+	}
+	else{
+		var alignStyle = "left";
+		var directionStyle = "ltr";
+	}
+	if(isset(options.content)){
+		var contentText = options.content;
+	}
+	else{
+		var contentText = "";
+	}
+	if($('.toast_material').length){
+		if($('.toast_open').length){
+			$('.toast_material').addClass("animated fadeOutDown");
+		}
+		else{
+			$('.toast_material').remove();
+		}	
+		$('body').append('<div class="toast_material toast_open animated fadeInUp" style="'+ positionStyle +';text-align:'+ alignStyle +'"><p style="direction:'+ directionStyle +'">'+ contentText +'</p></div>');
+		setTimeout(function() {
+			$('.toast_material').removeClass("fadeInUp");
+			$('.toast_material').addClass("fadeOutUp");
+		}, 3000);
+	}
+	else{
+		$('body').append('<div class="toast_material toast_open animated fadeInUp" style="'+ positionStyle +';text-align:'+ alignStyle +'"><p style="direction:'+ directionStyle +'">'+ contentText +'</p></div>');
+		setTimeout(function() {
+			$('.toast_material').removeClass("fadeInUp");
+			$('.toast_material').addClass("fadeOutUp");
+		}, 3000);
+	}
+};
